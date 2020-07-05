1e45bf76661776427ad0ee9950e5a8ac17c1c973
Jswig <andersjopo@gmail.com>
2020-07-05 15:33:54 -0700

    typoes

--- src/features/utilities.py
+++ src/features/utilities.py
@@ -46,12 +46,13 @@ def merge_features(df: pd.DataFrame)-> pd.DataFrame:
             
         )
         .drop( # features for which we created average estimates
-            ['ndvi_ne', 'ndvi_nw', 'ndvi_se', 'ndvi_sw',
-            'station_max_temp_c', 'reanalysis_max_air_temp_k',
-            'station_min_temp_c', 'reanalysis_min_air_temp_k',
-            'station_avg_temp_c', 'reanalysis_avg_temp_k', 'reanalysis_air_temp_k',
-            'station_precip_mm', 'reanalysis_sat_precip_amt_mm',
-            'station_diur_temp_rng_c', 'reanalysis_tdtr_k'
+            [
+                'ndvi_ne', 'ndvi_nw', 'ndvi_se', 'ndvi_sw',
+                'station_max_temp_c', 'reanalysis_max_air_temp_k',
+                'station_min_temp_c', 'reanalysis_min_air_temp_k',
+                'station_avg_temp_c', 'reanalysis_avg_temp_k', 'reanalysis_air_temp_k',
+                'station_precip_mm', 'reanalysis_sat_precip_amt_mm',
+                'station_diur_temp_rng_c', 'reanalysis_tdtr_k'
             ],
             axis = 1
         )
